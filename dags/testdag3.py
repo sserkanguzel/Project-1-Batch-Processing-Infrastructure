87d978240d0c621ed16ba07ae345a58c9d5066d3
Sertug Serkan Guzel <66708115+sserkanguzel@users.noreply.github.com>
2025-07-01 12:14:59 +0300

    Update testdag3.py

--- dags/testdag3.py
+++ dags/testdag3.py
@@ -6,7 +6,7 @@ def hello_world():
     print("Hello World")
 
 with DAG(
-    dag_id="simple_hello_world",
+    dag_id="simple_hello_world3",
     start_date=datetime(2025, 1, 1),
     schedule_interval=None,
     catchup=False,
